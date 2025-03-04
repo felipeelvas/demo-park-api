af1868fa9e4862f2796eea7032e1990a32fc40b8
Felipe Elvas Barbosa <elvasfelipe@hotmail.com>
2025-03-04 18:04:03 -0300

    Configurar o Timezone e o Locale padrão

---
+++ src/main/java/com/elvas/demo_park_api/config/SpringTimeZoneConfig.java
@@ -0,0 +1,16 @@
+package com.elvas.demo_park_api.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+class SpringTimeZoneConfig {
+
+    @PostConstruct
+    public void timeZoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
+}
